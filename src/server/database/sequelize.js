428425fc51a4a980839f73e6de8fab624d69a8cd
Stephen Wuh <swuh87@gmail.com>
2021-08-23 18:11:19 -0400

    database info included in .env

--- src/server/database/sequelize.js
+++ src/server/database/sequelize.js
@@ -1,6 +1,7 @@
 const Sequelize = require("sequelize");
+require('dotenv').config();
 
-const connection = new Sequelize('digitalCloset', 'postgres', 'postgres', {
+const connection = new Sequelize('digitalCloset', process.env.SEQUELIZE_DATABASE_USER_ID, process.env.SEQUELIZE_DATABASE_PW, {
     host: 'localhost',
     dialect: 'postgres',
     operatorsAliases: false
