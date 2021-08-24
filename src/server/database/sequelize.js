5545b528de7a7ec15c575cb63f3983c384bcb097
Stephen Wuh <swuh87@gmail.com>
2021-08-23 21:26:45 -0400

    database name included in .env file

--- src/server/database/sequelize.js
+++ src/server/database/sequelize.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 require('dotenv').config();
 
-const connection = new Sequelize('digitalCloset', process.env.SEQUELIZE_DATABASE_USER_ID, process.env.SEQUELIZE_DATABASE_PW, {
+const connection = new Sequelize(process.env.SEQUELIZE_DATABASE_NAME, process.env.SEQUELIZE_DATABASE_USER_ID, process.env.SEQUELIZE_DATABASE_PW, {
     host: 'localhost',
     dialect: 'postgres',
     operatorsAliases: false
